59a64433ade634d2c779d590f6b2b367a93c2393
kylin <kylin_lee@foxmail.com>
2016-05-22 13:32:13 +0800

    完成三型词法分析

---
+++ lexical_analysis.cpp
@@ -0,0 +1,437 @@
+#include "stdio.h"
+#include "iostream"
+#include "fstream"
+#include "string"
+#include "cstring"
+#include "set"
+using namespace std;
+
+#define MAX_FORM 200
+#define MAX_DFA 200
+#define MAX_END_WORD 200
+
+//MAX_FORM为最大生成式个数
+//MAX_DFA为DFA矩阵的最大行数,即节点个数
+//MAX_END_WORD为最大终结符个数
+
+struct Node
+{
+	char word;
+	int data;
+	Node* next;
+};
+
+struct NFA
+{
+	int size;//节点个数,不包含-1终态
+	int first_word;//入口
+	set<char> end_word;//终结符集合
+	Node* head;
+};
+
+struct DFA
+{
+	int w;//矩阵宽度
+	int h;//矩阵高度
+	set<int> end;
+	char word[MAX_END_WORD];
+	int x[MAX_DFA][MAX_END_WORD];
+};
+
+class grammar
+{
+	public:
+		int size;
+		int first_word;
+		set<char> start_word;
+		set<char> end_word;
+		char form[MAX_FORM][5];
+
+		bool is_start(char word)
+		{
+			if(start_word.find(word)!=start_word.end())
+				return true;
+			return false;
+		}
+		bool is_end(char word)
+		{
+			if(end_word.find(word)!=end_word.end())
+				return true;
+			return false;
+		}
+};
+
+//从文件读取一个文法
+grammar read_file(char* file_path)
+{
+	ifstream input_file;
+	grammar G;
+	G.size = 0;
+	char first_word;
+	char buf[5];
+	input_file.open(file_path);
+	input_file>>buf;
+	G.start_word.insert(buf[0]);
+	first_word=buf[0];
+	G.end_word.insert(buf[3]);
+	for(int i=0;i<5;i++)
+		G.form[G.size][i]=buf[i];
+	G.size++;
+	while(!input_file.eof())
+	{
+		input_file>>buf;
+		G.start_word.insert(buf[0]);
+		G.end_word.insert(buf[3]);
+		for(int i=0;i<5;i++)
+			G.form[G.size][i]=buf[i];
+		G.size++;
+	}
+	int count=0;
+	set<char>::iterator it;
+	for(it=G.start_word.begin();it!=G.start_word.end();it++)
+	{
+		if((*it)==first_word)
+		{
+			G.first_word=count;
+			break;
+		}
+		count++;
+	}
+	return G;
+}
+
+NFA nfa_builder(grammar G)
+{	
+	int n = G.start_word.size();
+	Node* _list = new Node[n];
+	NFA nf;
+	nf.size=n;
+	nf.first_word=G.first_word;
+	nf.end_word=G.end_word;
+	nf.head=_list;
+
+	int i=0;
+	char buf[5];
+	set<char>::iterator it1;
+	for(it1=G.start_word.begin();it1!=G.start_word.end();it1++)
+	{
+		Node* p= &_list[i];
+		p->next=NULL;
+		(*p).data=0;
+		for(int j=0;j<G.size;j++)
+		{
+			for(int ii=0;ii<5;ii++)
+				buf[ii]=G.form[j][ii];
+			if(buf[0]!=(*it1))
+				continue;
+			else
+			{
+				Node* q = new Node;
+				q->next = NULL;
+				(*q).data = -1;
+				int index=0;
+				set<char>::iterator it2;
+				for(it2=G.start_word.begin();it2!=G.start_word.end();it2++)
+				{
+					if((*it2)==buf[4])
+					{
+						(*q).data = index++;
+						break;
+					}
+					index++;
+				}
+				(*q).word=buf[3];
+				p->next=q;
+				p=q;
+				_list[i].data++;
+			}
+		}
+		i++;
+	}
+
+	// //测试NFA正确性
+	// Node* h1=nf.head;
+	// cout<<"first word:"<<nf.first_word<<endl;
+	// for(int i=0;i<n;i++)	
+	// {	
+	// 	Node* p1 = h1[i].next;
+	// 	for(int j=0;j<h1[i].data;j++)
+	// 	{
+	// 		cout<<(*p1).word<<":"<<(*p1).data<<" ";
+	// 		p1=p1->next;
+	// 	}
+	// 	cout<<endl;
+	// }
+
+	return nf;
+}
+
+set<int> move(const NFA nfx, char a, set<int> old, int &flag)
+{
+	int n=nfx.size;
+	//set<int> nt = old;
+	set<int> nt ;
+	Node* head=nfx.head;
+	for(int i=0;i<n;i++)		
+		if(old.find(i)!=old.end())//old集中含有i
+		{
+			Node* p = head[i].next;
+			for(int j=0;j<head[i].data;j++)
+			{
+				if((*p).word==a)
+				{
+					nt.insert((*p).data);
+					flag=1;//可行路径
+				}
+				p=p->next;
+			}
+		}
+	return nt;
+}
+
+set<set<int> >::iterator where(const set<set<int> > &c, const set<set<int> > &mark)
+{
+	set<set<int> >::iterator it;
+	for(it=c.begin();it!=c.end();it++)
+		if(mark.find(*it)==mark.end())
+			return it;
+	return c.end();
+}
+
+DFA dfa_builder(NFA nfx)
+{
+	DFA df;
+	df.w = nfx.end_word.size();
+	for(int i=0;i<MAX_DFA;i++)
+		for(int j=0;j<MAX_END_WORD;j++)
+			df.x[i][j]=-2;//表示无效路径
+	set<set<int> > c;
+	set<set<int> > cc;
+	set<int> t;
+	t.insert(nfx.first_word);
+	c.insert(t);
+	int sp=0;
+	set<int> sign[MAX_DFA];
+	sign[sp]=t;
+	sp++;
+	while(where(c,cc)!=c.end())
+	{
+		set<set<int> >::iterator it1;
+		it1=where(c,cc);
+		t=(*it1);
+		int cow=0;
+		for(int i=0;i<sp;i++)
+			if(sign[i]==t)
+			{
+				cow=i;
+				break;
+			}
+		cc.insert(t);//标记t
+		int col=0;
+		set<char>::iterator it2;
+		for(it2=nfx.end_word.begin();it2!=nfx.end_word.end();it2++)
+		{
+			df.word[col] = (*it2);
+			int flag=0;//环路标记
+			set<int> u = move(nfx, (*it2), t, flag);
+			if(c.find(u)==c.end())//u是新的
+			{
+				c.insert(u);//将u加入c
+				sign[sp]=u;//记下u的位置
+				df.x[cow][col]=sp;//构造DFA
+				if(u.find(-1)!=u.end())//判断是否为结束符
+					df.end.insert(sp);//记录结束符
+				sp++;
+			}
+			else if(flag==1)//u已存在于c中,且构成可行路径
+			{
+				for(int isp=0;isp<sp;isp++)
+				{
+					if(u==sign[isp])//找到u的位置
+					{
+						df.x[cow][col]=isp;//构造DFA环路
+						break;
+					}
+				}
+			}
+			col++;
+		}
+	}
+	df.h = sp;
+
+	// //测试DFA正确性
+	// for(int i=0;i<sp;i++)
+	// {
+	// 	for(int j=0;j<df.w;j++)
+	// 		cout<<df.x[i][j]<<" ";
+	// 	cout<<endl;
+	// }
+	return df;
+}
+
+bool is_dfa(DFA df, char s[])
+{
+	int p=0;
+	int end_index;
+	for(int i=0;s[i]!='\0';i++)
+	{
+		for(int j=0;j<df.w;j++)
+		{
+			if(s[i]==df.word[j])
+			{
+				p=df.x[p][j];
+				end_index=p;
+				if(i==0&&p<0)
+					return false;
+				break;
+			}
+			if(p<0||j==df.w-1)
+				return false;
+		}
+	}
+	if(df.end.find(end_index)==df.end.end())
+		return false;
+	return true;
+}
+
+bool is_identifier(char s[])
+{
+	char file_path[] = "input_identifier.txt";
+	grammar G = read_file(file_path);
+	NFA nfx = nfa_builder(G);
+	DFA df = dfa_builder(nfx);
+	if(is_dfa(df,s))
+		return true;
+	else
+		return false;
+}
+
+bool is_const(char s[])
+{
+	char file_path[] = "input_const.txt";
+	grammar G = read_file(file_path);
+	NFA nfx = nfa_builder(G);
+	DFA df = dfa_builder(nfx);
+	if(is_dfa(df,s))
+		return true;
+	else
+		return false;
+}
+
+bool is_key_word(string s)
+{
+	if(s=="int"||s=="for"||s=="double"||s=="#include"||s=="using"||s=="namespace"||s=="std"||s=="return"||s=="main"||s=="void"||s=="endl"||s=="cout"||s=="cin")
+		return true;
+	return false;
+}
+
+bool is_limiter(string s)
+{
+	if(s=="{")
+		return true;
+	else if(s=="}")
+		return true;
+	else if(s=="(")
+		return true;
+	else if(s==")")
+		return true;
+	else if(s==";")
+		return true;
+	else if(s==",")
+		return true;
+	else
+		return false;
+}
+
+bool is_operator(string s)
+{
+	if(s=="+"||s=="-"||s=="++"||s=="<"||s==">"||s=="*"||s=="--"||s==">="||s=="<="||s=="="||s=="\""||s=="+="||s=="<<"||s==">>")
+		return true;
+	return false;
+}
+
+bool is_empty(string s)
+{
+	if(s==""||s==" ")
+		return true;
+	return false;
+}
+
+int what_this(string str)
+{
+	char temp[100];
+	char *cr=temp;
+	strcpy(cr,str.c_str());
+	for(int i=0;cr[i]!='\0';i++)
+	{
+		if((cr[i]>='a'&&cr[i]<='z')||(cr[i]>='A'&&cr[i]<='Z'))
+			cr[i]='l';
+		if(cr[i]>='0'&&cr[i]<='9')
+			cr[i]='d';
+	}
+	string s=str;
+	if(is_limiter(s))
+		return 1;
+	if(is_operator(s))
+		return 2;
+	if(is_key_word(s))
+		return 3;
+	if(is_empty(s))
+		return 4;
+	if(is_const(cr))
+		return 5;
+	if(is_identifier(cr))
+		return 6;
+	return 0;
+}
+
+string read_code(string str)
+{
+	int token = what_this(str);
+	string print_word;
+	switch(token)
+	{
+	case 1:
+		print_word="[limiter]";
+		break;
+	case 2:
+		print_word="[operator]";
+		break;
+	case 3:
+		print_word="[key_word]";
+		break;
+	case 4:
+		print_word=" ";
+		break;
+	case 5:
+		print_word="[const]";
+		break;
+	case 6:
+		print_word="[identifier]";
+		break;
+	default:
+		print_word="[error_word]";
+	}
+	return print_word;
+}
+
+void print_token()
+{
+	ifstream input_file;
+	fstream output_file("token_table.txt", ios::out);
+	input_file.open("source_code.txt");//读取源代码
+	string buf;
+	while(!input_file.eof())
+	{
+		input_file>>buf;
+		output_file<<buf<<' '<<read_code(buf)<<endl;
+	}
+	input_file.close();
+	output_file.close();
+}
+
+int main()
+{
+	print_token();
+	return 0;
+}
---
+++ phaser_analysis.cpp
@@ -0,0 +1,309 @@
+#include "stdio.h"
+#include "iostream"
+#include "fstream"
+#include "string"
+#include "cstring"
+#include "set"
+#include "vector"
+#include "map"
+using namespace std;
+
+struct word
+{
+	int type;
+	string value;
+};
+
+struct form
+{
+	string start;
+	string end;
+};
+
+vector<word> read_token(char* file_path)
+{
+	ifstream token_file;
+	token_file.open(file_path);
+	string buf_1,buf_2;
+	vector<word> v;
+	while(!token_file.eof())
+	{
+		word new_word;
+		token_file>>buf_1>>buf_2;
+		new_word.value = buf_1;
+		if(buf_2=="[key_word]")
+			new_word.type = 1;
+		else if(buf_2=="[identifier]")
+			new_word.type = 2;
+		else if(buf_2=="[operator]")
+			new_word.type = 3;
+		else if(buf_2=="[limiter]")
+			new_word.type = 4;
+		else if(buf_2=="[const]")
+			new_word.type = 5;
+		else
+			new_word.type = 0;
+		v.push_back(new_word);
+	}
+	return v;
+}
+
+vector<form> read_grammar(char* file_path)
+{
+	ifstream grammar_file;
+	grammar_file.open(file_path);
+	string str;
+	vector<form> v;
+	while(!grammar_file.eof())
+	{
+		grammar_file>>str;
+		form buf;
+		buf.start = str.substr(0,1);
+		buf.end = str.substr(3,str.npos);
+		v.push_back(buf);
+	}
+	return v;
+}
+
+vector<string> split(const string str)
+{
+	vector<string> result;
+	for(int i=0;i<str.size();i++)
+	{
+		string temp = str.substr((string::size_type)i,(string::size_type)(i+1));
+		result.push_back(temp);
+	}
+	return result;
+}
+
+set<string> find_vn(const vector<form> f)
+{
+	set<string> vn;
+	int n = f.size();
+	for(int i=0;i<n;i++)
+	{
+		string buf = f[i].start;
+		vn.insert(buf);
+	}
+	return vn;
+}
+
+set<string> find_vt(const vector<form> f)
+{
+	set<string> vt;
+	set<string> vn;
+	vn = find_vn(f);
+	int n = f.size();
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<f[i].end.size();j++)
+		{
+			string temp = f[i].end.substr((string::size_type)j,(string::size_type)(j+1));
+			if(vn.find(temp)==vn.end())
+				vt.insert(temp);
+		}
+	}
+	if(vt.find("$")!=vt.end())
+		vt.erase("$");
+	return vt;
+}
+
+map<string,int>  get_empty(const vector<form> v_value)//求能推出空的非终结符
+{
+	vector<form> v;
+	v = v_value;
+	map<string,int> empty;
+	set<string> vt = find_vt(v);
+	set<string> vn = find_vn(v);
+	set<string>::iterator it1;
+	for(it1=vn.begin();it1!=vn.end();it1++)
+		empty.insert(pair<string,int>((*it1),-1));
+	vector<form>::iterator it2;
+	for(it2=v.begin();it2!=v.end();it2++)//删除所有右部含有终结符的产生式
+	{
+		vector<string> right = split((*it2).end);//拆分产生式右部
+		int right_size = right.size();
+		bool flag = false;
+		for(int j=0;j<right_size;j++)
+			if(vt.find(right.at(j))!=vt.end())//产生式右部含有终结符
+			{
+				flag = true;
+				break;
+			}
+		if(flag)
+		{
+			int count = 0;
+			vector<form>::iterator it4;
+			for(it4=v.begin();it4!=v.end();it4++)
+				if((*it4).start==(*it2).start)
+					count++;
+			if(count == 1)
+				empty[(string)(*it2).start] = 0;
+			v.erase(it2);
+			it2--;//因为删除了一个产生式,所以迭代器要前移,否则会错过后一个值
+		}
+		
+	}
+
+	for(it2=v.begin();it2!=v.end();it2++)
+	{
+		if((*it2).end=="$")
+		{
+			string temp = (*it2).start;
+			empty[(string)(*it2).start] = 1;//能推出空
+			vector<form>::iterator it3;
+			for(it3=v.begin();it3!=v.end();it3++)
+			{
+				if((*it3).start==temp)
+				{
+					v.erase(it3);//删除该终结符的所有产生式
+					it3--;
+					if(it2!=v.begin())
+						it2--;//因为删除了一个产生式,所以迭代器要前移,否则会错过后一个值
+				}
+			}
+		}
+	}
+
+	while(v.size()!=0)
+	{
+		for(it2=v.begin();it2!=v.end();it2++)
+		{
+			vector<string> right = split((*it2).end);//拆分产生式右部
+			int right_size = right.size();
+			bool flag = false;
+			for(int j=0;j<right_size;j++)
+			{
+				if(empty[(string)right.at(j)]==0)//产生式右部含有终结符
+				{
+					flag = true;
+					break;
+				}
+				else if(empty[(string)right.at(j)]==1)
+				{
+					right.at(j)="";
+				}
+			}
+			for(int j=right_size-1;j>0;j--)
+				right.at(j-1) += right.at(j);
+			(*it2).end = right.at(0);
+			if((*it2).end=="")
+			{
+				string temp = (*it2).start;
+				empty[(string)(*it2).start] = 1;//能推出空
+				vector<form>::iterator it3;
+				for(it3=v.begin();it3!=v.end();it3++)
+				{
+					if((*it3).start==temp)
+					{
+						v.erase(it3);//删除该终结符的所有产生式
+						it3--;
+						if(it2!=v.begin())
+							it2--;//因为删除了一个产生式,所以迭代器要前移,否则会错过后一个值
+					}
+				}
+			}
+			if(flag)
+			{
+				int count = 0;
+				vector<form>::iterator it4;
+				for(it4=v.begin();it4!=v.end();it4++)
+					if((*it4).start==(*it2).start)
+						count++;
+				if(count == 1)
+					empty[(string)(*it2).start] = 0;
+				v.erase(it2);
+				it2--;//因为删除了一个产生式,所以迭代器要前移,否则会错过后一个值
+			}
+			
+		}
+	}
+	//测试empty表是否正确
+	map<string,int>::iterator it;
+	for(it=empty.begin();it!=empty.end();it++)
+ 	cout<<it->first<<":"<<it->second<<endl;
+	return empty;
+}
+
+set<string> _union(const set<string > a , const set<string> b)//求并集
+{
+	set<string> result;
+	result = a;
+	set<string>::iterator it;
+	for(it=b.begin();it!=b.end();it++)
+		result.insert(*it);
+	return result;
+}
+
+set<string> get_first(string symbol , set<string > vt , set<string > vn , vector<form > f , map<string,int > empty)
+{
+	set<string> result;
+	if(vt.find(symbol)!=vt.end())
+	{
+		result.insert(symbol);
+		return result;
+	}
+	int flag=0;
+	vector<form>::iterator it;
+	for(it=f.begin();it!=f.end();it++)
+	{
+		if((*it).start==symbol)
+		{
+			if((*it).end=="$")
+			{
+				flag = 1;
+				continue;
+			}
+			vector<string> right = split((*it).end);
+			int right_size = right.size();
+			for(int i=0;i<right_size;i++)
+			{
+				if(result.find("$")!=result.end()&&flag==0)
+					result.erase("$");
+				result = _union(result , get_first(right.at(i) , vt , vn , f , empty));
+				if(empty[(string)right.at(i)]==0||vt.find(right.at(i))!=vt.end())
+					break;
+				else if(i==right_size-1)
+					flag = 1;
+			}
+		}
+		if(flag==1)
+			result.insert("$");
+	}
+	return result;
+}
+
+map<string,set<string> > make_first(const set<string> vt , const set<string> vn , const vector<form> f , const map<string,int> empty)
+{
+	map<string,set<string> > result;
+	set<string>::iterator it1;
+	for(it1=vt.begin();it1!=vt.end();it1++)
+	{
+		set<string> buf;
+		buf.insert((*it1));
+		result.insert(pair<string,set<string> >((*it1),buf));
+	}
+	for(it1=vn.begin();it1!=vn.end();it1++)
+	{
+		set<string> buf;
+		buf = get_first((*it1),vt,vn,f,empty);
+		result.insert(pair<string,set<string> >((*it1),buf));
+	}
+	return result;
+}
+
+int main()
+{
+	char s[] = "input_grammar.txt";
+	vector<form> v = read_grammar(s);
+	set<string> vt = find_vt(v);
+	set<string> vn = find_vn(v);
+	map<string,int> empty = get_empty(v);
+	map<string,set<string> > first,follow,select;
+	first = make_first(vt,vn,v,empty);
+
+	set<string>::iterator it;
+	for(it=first["D"].begin();it!=first["D"].end();it++)
+		cout<<*it<<" ";
+	return 0;
+}
